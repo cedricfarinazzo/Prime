4d09ba9d2c8ffab9c2aeef0781b4889ebc8916cb
cedricfarinazzo <34288081+cedricfarinazzo@users.noreply.github.com>
2017-12-29 00:52:08 +0100

    Add python files

---
+++ config.py
@@ -0,0 +1,7 @@
+############# CONFIG DATA ###############
+
+# Please change path to your current folder to generate or read a file 
+# and name to the name of the file that you want to create or read
+
+path = ""
+name = ""
